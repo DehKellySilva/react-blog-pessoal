8fccb7b48f77254e8d96d7d6921a6b9b3c0aa6a5
Denise <denisekellypereira@hotmail.com>
2022-03-21 23:39:14 -0300

    React

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+function Home(){
+    return (
+    <>
+        <h1 className='titulo'>Teste</h1>
+        <img src="http://s2.glbimg.com/h3Duok3KWVA8yaIOzZZIESkNLC4DKPsVVGWWhNMHhpNIoz-HdGixxa_8qOZvMp3w/e.glbimg.com/og/ed/f/original/2013/08/02/imagem_para_sexta_51.jpg" alt="Imagem Tela Inicial" width="500" height="500"/>
+    </>
+    );
+}
+
+export default Home;
